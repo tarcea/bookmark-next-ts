c652eb1fa7b7ef5c46d3bac17270268481efb7c5
tarcea <tarcea@gmail.com>
2022-08-02 21:42:38 +0200

    error handler added

--- components/Message.tsx
+++ components/Message.tsx
@@ -19,7 +19,10 @@ const Message = () => {
   }, [message, setMessage]);
   // if (!visible) return null;
   return visible ? (
-    <div className='fixed top-20 left-1/2 -translate-y-1/2 -translate-x-1/2 text-center bg-rose-300 rounded w-80 z-40 text-s p-5 opacity-70 md:w-96'>
+    <div
+      className='fixed top-20 left-1/2 -translate-y-1/2 -translate-x-1/2 text-center bg-rose-300 rounded w-80 z-40 text-s p-5 opacity-70 md:w-96'
+      onClick={() => setVisible(false)}
+    >
       {message}
     </div>
   ) : null;
